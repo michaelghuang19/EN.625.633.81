24a69a8064a9afe1926fb221fbdaa89cf7cb75a5
Michael Huang <michaelghuang19@gmail.com>
2026-04-20 20:13:21 -0400

    Exam 2 Problems 2, 4, 5 Complete

---
+++ Exam 2/problem_4_antithetic.R
@@ -0,0 +1,27 @@
+n = 1000
+nmbSimulations = 1000
+
+antithetic_comparison = function(n, nmbSimulations) {
+  brute_force_means = rep(0, nmbSimulations)
+  antithetic_means = rep(0, nmbSimulations)
+    
+  for (i in 1:nmbSimulations) {
+    # Brute force, simulate using inverse-transform directly
+    U = runif(2 * n)
+    X = 1 / sqrt(1 - U)
+    brute_force_means[i] = mean(X)
+    
+    # Antithetic approach in pairs
+    U = runif(n)
+    X = 1 / sqrt(1 - U)
+    Y = 1 / sqrt(U)
+    antithetic_means[i] = mean((X + Y) / 2)
+  }
+      
+  print(paste("Brute force mean estimate mean:", mean(brute_force_means)))
+  print(paste("Brute force mean estimate variance:", var(brute_force_means)))
+  print(paste("Antithetic mean estimate mean:", mean(antithetic_means)))
+  print(paste("Antithetic mean estimate variance:", var(antithetic_means)))
+}
+
+antithetic_comparison(n, nmbSimulations)
